991282de3584e15144be182567574d2051e53d5f
Andrew Bialecki <andrew.bialecki@gmail.com>
2012-07-11 15:47:55 -0400

    Initial commit.

---
+++ klaviyo.gemspec
@@ -0,0 +1,18 @@
+files = ['klaviyo.gemspec', '{lib}/**/*'].map {|f| Dir[f]}.flatten
+
+Gem::Specification.new do |s|
+  s.name        = 'klaviyo'
+  s.version     = '0.9.0'
+  s.date        = '2012-07-10'
+  s.summary     = 'Ruby wrapper for the Klaviyo API'
+  s.description = 'You heard us, a Ruby wrapper for the Klaviyo API'
+  s.authors     = ['Andrew Bialecki']
+  s.email       = 'andrew@klaviyo.com'
+  s.files       = files
+  s.homepage = 'http://www.klaviyo.com/'
+  s.require_path = 'lib'
+  s.has_rdoc    = false
+  s.add_dependency 'json'
+  s.add_dependency 'rack'
+  s.add_dependency 'escape'
+end
---
+++ lib/klaviyo/client.rb
@@ -0,0 +1,73 @@
+require 'open-uri'
+require 'base64'
+require 'json'
+require 'klaviyo/client/middleware'
+
+module Klaviyo
+  class KlaviyoError < StandardError; end
+  
+  class Client
+    def initialize(api_key, env, url = 'http://a.klaviyo.com/')
+      @api_key = api_key
+      @env = env
+      @url = url
+    end
+    
+    def track(event, kwargs = {})
+      defaults = {:id => nil, :email => nil, :properties => {}, :customer_properties => {}}
+      kwargs = defaults.merge(kwargs)
+      
+      if kwargs[:email].to_s.empty? and kwargs[:id].to_s.empty?
+        raise KlaviyoError.new('You must identify a user by email or ID')
+      end
+      
+      customer_properties = kwargs[:customer_properties]
+      customer_properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
+      customer_properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
+      
+      params = build_params({
+        :token => @api_key,
+        :event => event,
+        :properties => kwargs[:properties],
+        :customer_properties => customer_properties,
+        :ip => ''
+      })
+      request('crm/api/track', params)
+    end
+    
+    def track_once(event, customer_properties = {}, properties = {})
+      properties.update('__track_once__' => true)
+      track(event, email, id, properties, customer_properties)
+    end
+    
+    def identify(kwargs = {})
+      defaults = {:id => nil, :email => nil, :properties => {}}
+      kwargs = defaults.merge(kwargs)
+      
+      if kwargs[:email].to_s.empty? and kwargs[:id].to_s.empty?
+        raise KlaviyoError.new('You must identify a user by email or ID')
+      end
+      
+      properties = kwargs[:properties]
+      properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
+      properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
+
+      params = build_params({
+        :token => @api_key,
+        :properties => properties
+      })
+      request('crm/api/identify', params)
+    end
+
+    private
+    
+    def build_params(params)
+      'data=' + Base64.encode64(JSON.generate(params)).gsub(/\n/,'')
+    end
+    
+    def request(path, params)
+        url = @url + path + '?' + params
+        open(url).read == '1' ? true : false
+    end
+  end
+end
