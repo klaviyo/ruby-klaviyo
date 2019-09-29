567285aceda4e4b7e14516f26085e7ed1db038d6
jordan allain <jordan.allain@klaviyo.com>
2019-09-29 15:21:38 -0400

    Include 'api/' in base url
    - Moved url segment out of method calls and into the base url to reduce
      duplication.

--- lib/klaviyo/client.rb
+++ lib/klaviyo/client.rb
@@ -4,21 +4,21 @@ require 'json'
 
 module Klaviyo
   class KlaviyoError < StandardError; end
-  
+
   class Client
-    def initialize(api_key, url = 'http://a.klaviyo.com/')
+    def initialize(api_key, url = 'http://a.klaviyo.com/api/')
       @api_key = api_key
       @url = url
     end
-    
+
     def track(event, kwargs = {})
       defaults = {:id => nil, :email => nil, :properties => {}, :customer_properties => {}, :time => nil}
       kwargs = defaults.merge(kwargs)
-      
+
       if kwargs[:email].to_s.empty? and kwargs[:id].to_s.empty?
         raise KlaviyoError.new('You must identify a user by email or ID')
       end
-      
+
       customer_properties = kwargs[:customer_properties]
       customer_properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
       customer_properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
@@ -31,24 +31,24 @@ module Klaviyo
         :ip => ''
       }
       params[:time] = kwargs[:time].to_time.to_i if kwargs[:time]
-     
+
       params = build_params(params)
-      request('api/track', params)
+      request('track', params)
     end
-    
+
     def track_once(event, opts = {})
       opts.update('__track_once__' => true)
       track(event, opts)
     end
-    
+
     def identify(kwargs = {})
       defaults = {:id => nil, :email => nil, :properties => {}}
       kwargs = defaults.merge(kwargs)
-      
+
       if kwargs[:email].to_s.empty? and kwargs[:id].to_s.empty?
         raise KlaviyoError.new('You must identify a user by email or ID')
       end
-      
+
       properties = kwargs[:properties]
       properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
       properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
@@ -57,15 +57,15 @@ module Klaviyo
         :token => @api_key,
         :properties => properties
       })
-      request('api/identify', params)
+      request('identify', params)
     end
 
     private
-    
+
     def build_params(params)
       "data=#{CGI.escape Base64.encode64(JSON.generate(params)).gsub(/\n/,'')}"
     end
-    
+
     def request(path, params)
       url = "#{@url}#{path}?#{params}"
       open(url).read == '1'
