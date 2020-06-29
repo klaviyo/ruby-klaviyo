71817918bf4f4e51b02bfc932e8f2e55b1c98141
Chuck <chuck.curtis@klaviyo.com>
2020-06-29 11:32:02 -0400

    Update spacing

--- lib/klaviyo/client.rb
+++ lib/klaviyo/client.rb
@@ -34,134 +34,108 @@ module Klaviyo
     end
 
 # METRICS API
-
     def get_metrics(kwargs = {})
       path = @metrics
-
       v1_request('GET', path, kwargs)
     end
 
     def get_metrics_timeline(kwargs = {})
       path = "#{@metrics}/#{@timeline}"
-
       v1_request('GET', path, kwargs)
     end
 
     def get_specific_metric_timeline(metric_id, kwargs = {})
       path = "#{@metric}/#{metric_id}/#{@timeline}"
-
       v1_request('GET', path, kwargs)
     end
 
 # PROFILES API
-
     def get_person_attributes(person_id)
       path = "#{@person}/#{person_id}"
-
       v1_request('GET', path)
     end
 
     def update_person_attributes(person_id, kwargs = {})
       path = "#{@person}/#{person_id}"
-
       v1_request('PUT', path, kwargs)
     end
 
     def get_person_metrics_timeline(person_id, kwargs = {})
       path = "#{@person}/#{person_id}/#{@metrics}/#{@timeline}"
-
       v1_request('GET', path, kwargs)
     end
 
     def get_person_metric_timeline(person_id, metric_id, kwargs = {})
       path = "#{@person}/#{person_id}/#{@metric}/#{metric_id}/#{@timeline}"
-
       v1_request('GET', path, kwargs)
     end
 
 # LISTS API
-
     def create_list(list_name)
       path = "#{@lists}"
-
       body = {
         "list_name": list_name
       }
-
       v2_request('POST', path, body)
     end
 
     def get_lists()
       path = "#{@lists}"
-
       v2_request('GET', path)
     end
 
     def get_list_details(list_id)
       path = "#{@list}/#{list_id}"
-
       v2_request('GET', path)
     end
 
     def update_list_details(list_id, list_name)
       path = "#{@list}/#{list_id}"
-
       body = {
         "list_name": list_name
       }
-
       v2_request('PUT', path, body)
     end
 
     def delete_list(list_id)
       path = "#{@list}/#{list_id}"
-
       v2_request('DELETE', path)
     end
 
 # LIST SUBSCRIPTIONS
-
     def check_list_subscriptions(list_id, kwargs = {})
       path = "#{@list}/#{list_id}/#{@subscribe}"
-
       v2_request('GET', path, kwargs)
     end
 
     def unsubscribe_from_list(list_id, kwargs = {})
       path = "#{@list}/#{list_id}/#{@subscribe}"
-
       v2_request('DELETE', path, kwargs)
     end
 
 # LIST MEMBERSHIPS
-
     def add_to_list(list_id, kwargs = {})
       path = "#{@list}/#{list_id}/#{@members}"
-
       v2_request('POST', path, kwargs)
     end
 
     def check_list_memberships(list_id, kwargs = {})
       path = "#{@list}/#{list_id}/#{@members}"
-
       v2_request('GET', path, kwargs)
     end
 
     def remove_from_list(list_id, kwargs = {})
       path = "#{@list}/#{list_id}/#{@members}"
-
       v2_request('DELETE', path, kwargs)
     end
 
     def get_list_exclusions(list_id, kwargs = {})
       path = "#{@list}/#{list_id}/#{@exclusions}/#{@all}"
-
       v2_request('GET', path, kwargs)
     end
 
     def get_group_members(list_id)
       path = "#{@group}/#{list_id}/#{@members}/#{@all}"
-
       v2_request('GET', path)
     end
 
@@ -222,88 +196,67 @@ module Klaviyo
       if path == 'track' || path == 'identify'
         params = build_params(kwargs)
         url = "#{@domain}/#{path}?#{params}"
-
         res = Faraday.get(url)
 
       elsif kwargs[:body]
-
         check_private_api_key_exists()
-
         url = "#{@domain}/#{path}"
 
         if method == 'GET'
-
           data = kwargs[:body].to_json
-
           res = Faraday.get(url) do |req|
             req.body = data
             req.headers['Content-Type'] = 'application/json'
           end
 
         elsif method == 'POST'
-
           data = kwargs[:body].to_json
-
           res = Faraday.post(url) do |req|
             req.body = data
             req.headers['Content-Type'] = 'application/json'
           end
 
         elsif method == 'PUT'
-
           res = Faraday.put(url) do |req|
             req.body = data
             req.headers['Content-Type'] = 'application/json'
           end
 
         elsif method == 'DELETE'
-
           res = Faraday.delete(url) do |req|
             req.body = data
             req.headers['Content-Type'] = 'application/json'
           end
-
         end
 
       elsif method == 'GET'
-
         url = "#{@domain}/#{path}"
-
         res = Faraday.get(url)
 
       elsif method == 'PUT'
-
         url = "#{@domain}/#{path}"
-
         res = Faraday.put(url)
 
       end
     end
 
     def v1_request(method, path, kwargs = {})
-
       check_private_api_key_exists()
-
       defaults = {:page => nil, :count => nil, :since => nil, :sort => nil}
       params = defaults.merge(kwargs)
       query_params = encode_params(params)
-
       url_params = "api_key=#{@private_api_key_param}#{query_params}"
       full_path = "#{@v1}/#{path}?#{url_params}"
-
       request(method, full_path)
     end
 
     def v2_request(method, path, kwargs = {})
       path = "#{@v2}/#{path}"
-
       key = {
         "api_key": "#{@private_api_key}"
       }
-
       data = {}
       data[:body] = key.merge(kwargs)
-
       request(method, path, data)
     end
 
