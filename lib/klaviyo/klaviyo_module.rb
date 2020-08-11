a8b153da70c75a3758fdae472d2adcd84c9e8a66
Chuck <chuck.curtis@klaviyo.com>
2020-08-11 16:27:26 -0400

    Add Klaviyo module to its own file

---
+++ lib/klaviyo/klaviyo_module.rb
@@ -0,0 +1,22 @@
+require 'open-uri'
+require 'base64'
+require 'json'
+require 'Faraday'
+
+require_relative './client'
+require_relative 'apis/identify'
+require_relative 'apis/lists'
+require_relative 'apis/metrics'
+require_relative 'apis/profiles'
+require_relative 'apis/track'
+
+module Klaviyo
+  class << self
+    attr_accessor :api_key
+    attr_accessor :private_api_key
+  end
+
+  class KlaviyoError < StandardError; end
+
+  NO_PRIVATE_API_KEY_ERROR = 'Please provide Private API key for this request'
+end
