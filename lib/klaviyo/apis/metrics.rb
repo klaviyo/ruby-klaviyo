1e85aa5c4061463c49a283045e6611535a08e22d
Chuck <chuck.curtis@klaviyo.com>
2020-10-08 18:22:12 -0400

    Add metric export, require date

--- lib/klaviyo/apis/metrics.rb
+++ lib/klaviyo/apis/metrics.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module Klaviyo
   class Metrics < Client
     @@metric = 'metric'
@@ -47,6 +49,36 @@ module Klaviyo
         :since => since,
         :count => count,
         :sort => sort
+      }
+      Client.v1_request('GET', path, params)
+    end
+
+    # Export event data, optionally filtering and segmented on available event properties
+    # @param start_date [String] Beginning of the timeframe to pull event data for.  Default is 1 month ago
+    # @param end_date [String] End of the timeframe to pull event data for.  Default is the current day
+    # @param unit [String] Granularity to bucket data points into - one of ‘day’, ‘week’, or ‘month’. Defaults to ‘day’.
+    # @param measurement [String or JSON-encoded list] Type of metric to fetch
+    # @param where [JSON-encoded list] Conditions to use to filter the set of events. A max of 1 condition can be given.
+    # @param by [String] The name of a property to segment the event data on. Where and by parameters cannot be specified at the same time.
+    # @param count [Integer] Maximum number of segments to return. The default value is 25.
+    def self.get_metric_export(metric_id,
+                               start_date = Date.today.prev_month.to_s,
+                               end_date = Time.now.to_s.split(' ')[0],
+                               unit = 'day',
+                               measurement = 'count',
+                               where = nil,
+                               by = nil,
+                               count = 25
+                               )
+      path = "#{@@metric}/#{metric_id}/#{@@export}"
+      params = {
+        :start_date => start_date,
+        :end_date => end_date,
+        :unit => unit,
+        :measurement => measurement,
+        :where => where,
+        :by => by,
+        :count => count
       }
       Client.v1_request('GET', path, params)
     end
