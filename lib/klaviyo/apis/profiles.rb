75bc9020af337cd07dfc952a13edd50f015b2903
Chuck <chuck.curtis@klaviyo.com>
2020-10-12 16:35:04 -0400

    Update profiles constants

--- lib/klaviyo/apis/profiles.rb
+++ lib/klaviyo/apis/profiles.rb
@@ -1,27 +1,27 @@
 module Klaviyo
   class Profiles < Client
-    @@metric = 'metric'
-    @@metrics = 'metrics'
-    @@person = 'person'
-    @@timeline = 'timeline'
+    METRIC = 'metric'
+    METRICS = 'metrics'
+    PERSON = 'person'
+    TIMELINE = 'timeline'
 
     def self.get_person_attributes(person_id)
-      path = "#{@@person}/#{person_id}"
+      path = "#{PERSON}/#{person_id}"
       v1_request('GET', path)
     end
 
     def self.update_person_attributes(person_id, kwargs = {})
-      path = "#{@@person}/#{person_id}"
+      path = "#{PERSON}/#{person_id}"
       v1_request('PUT', path, kwargs)
     end
 
     def self.get_person_metrics_timeline(person_id, kwargs = {})
-      path = "#{@@person}/#{person_id}/#{@@metrics}/#{@@timeline}"
+      path = "#{PERSON}/#{person_id}/#{METRICS}/#{timeline}"
       v1_request('GET', path, kwargs)
     end
 
     def self.get_person_metric_timeline(person_id, metric_id, kwargs = {})
-      path = "#{@@person}/#{person_id}/#{@@metric}/#{metric_id}/#{@@timeline}"
+      path = "#{PERSON}/#{person_id}/#{METRIC}/#{metric_id}/#{TIMELINE}"
       v1_request('GET', path, kwargs)
     end
   end
