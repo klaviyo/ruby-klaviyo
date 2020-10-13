e348582cb8ca15fca0cca73df5192d72b2e29a14
Chuck <chuck.curtis@klaviyo.com>
2020-10-13 16:52:49 -0400

    Remove class variables, replace with constants for profiles

--- lib/klaviyo/apis/profiles.rb
+++ lib/klaviyo/apis/profiles.rb
@@ -1,10 +1,5 @@
 module Klaviyo
   class Profiles < Client
-    METRIC = 'metric'
-    METRICS = 'metrics'
-    PERSON = 'person'
-    TIMELINE = 'timeline'
-
     def self.get_person_attributes(person_id)
       path = "#{PERSON}/#{person_id}"
       v1_request('GET', path)
@@ -12,17 +7,17 @@ module Klaviyo
 
     def self.update_person_attributes(person_id, kwargs = {})
       path = "#{PERSON}/#{person_id}"
-      v1_request('PUT', path, kwargs)
+      v1_request(HTTP_PUT, path, kwargs)
     end
 
     def self.get_person_metrics_timeline(person_id, kwargs = {})
       path = "#{PERSON}/#{person_id}/#{METRICS}/#{timeline}"
-      v1_request('GET', path, kwargs)
+      v1_request(HTTP_GET, path, kwargs)
     end
 
     def self.get_person_metric_timeline(person_id, metric_id, kwargs = {})
       path = "#{PERSON}/#{person_id}/#{METRIC}/#{metric_id}/#{TIMELINE}"
-      v1_request('GET', path, kwargs)
+      v1_request(HTTP_GET, path, kwargs)
     end
   end
 end
