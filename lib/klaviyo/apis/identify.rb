23165b378d853009f4590360901fe95a2aaf8c75
Chuck <chuck.curtis@klaviyo.com>
2020-10-20 17:45:14 -0400

    Remove klaviyo module from all classes except client

--- lib/klaviyo/apis/identify.rb
+++ lib/klaviyo/apis/identify.rb
@@ -1,28 +1,26 @@
-module Klaviyo
-  class Identify < Client
-    # Used for identifying customers and managing profile properties
-    #
-    # @kwarg :id [String] the customer or profile id
-    # @kwarg :email [String] the customer or profile email
-    # @kwarg :properties [Hash] properties of the profile to add or update
-    def self.identify(kwargs = {})
-      defaults = {:id => nil, :email => nil, :properties => {}}
-      kwargs = defaults.merge(kwargs)
+class Identify < Client
+  # Used for identifying customers and managing profile properties
+  #
+  # @kwarg :id [String] the customer or profile id
+  # @kwarg :email [String] the customer or profile email
+  # @kwarg :properties [Hash] properties of the profile to add or update
+  def self.identify(kwargs = {})
+    defaults = {:id => nil, :email => nil, :properties => {}}
+    kwargs = defaults.merge(kwargs)
 
-      if kwargs[:email].to_s.empty? and kwargs[:id].to_s.empty?
-        raise KlaviyoError.new('You must identify a user by email or ID')
-      end
-
-      properties = kwargs[:properties]
-      properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
-      properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
-
-      params = {
-        :token => @api_key,
-        :properties => properties
-      }
-
-      request('GET', 'identify', params)
+    if kwargs[:email].to_s.empty? and kwargs[:id].to_s.empty?
+      raise KlaviyoError.new('You must identify a user by email or ID')
     end
+
+    properties = kwargs[:properties]
+    properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
+    properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
+
+    params = {
+      :token => @api_key,
+      :properties => properties
+    }
+
+    request('GET', 'identify', params)
   end
 end
--- lib/klaviyo/apis/track.rb
+++ lib/klaviyo/apis/track.rb
@@ -1,39 +1,37 @@
-module Klaviyo
-  class Track < Client
-    # Used for tracking events and customer behaviors
-    #
-    # @param event [String] the event to track
-    # @kwarg :id [String] the customer or profile id
-    # @kwarg :email [String] the customer or profile email
-    # @kwarg :properties [Hash] properties of the event
-    # @kwargs :customer_properties [Hash] properties of the customer or profile
-    # @kwargs :time [Integer] timestamp of the event
-    def self.track(event, kwargs = {})
-      defaults = {:id => nil, :email => nil, :properties => {}, :customer_properties => {}, :time => nil}
-      kwargs = defaults.merge(kwargs)
+class Track < Client
+  # Used for tracking events and customer behaviors
+  #
+  # @param event [String] the event to track
+  # @kwarg :id [String] the customer or profile id
+  # @kwarg :email [String] the customer or profile email
+  # @kwarg :properties [Hash] properties of the event
+  # @kwargs :customer_properties [Hash] properties of the customer or profile
+  # @kwargs :time [Integer] timestamp of the event
+  def self.track(event, kwargs = {})
+    defaults = {:id => nil, :email => nil, :properties => {}, :customer_properties => {}, :time => nil}
+    kwargs = defaults.merge(kwargs)
 
-      if kwargs[:email].to_s.empty? and kwargs[:id].to_s.empty?
-        raise KlaviyoError.new('You must identify a user by email or ID')
-      end
-
-      customer_properties = kwargs[:customer_properties]
-      customer_properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
-      customer_properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
-
-      params = {
-        :token => @api_key,
-        :event => event,
-        :properties => kwargs[:properties],
-        :customer_properties => customer_properties
-      }
-      params[:time] = kwargs[:time].to_time.to_i if kwargs[:time]
-
-      request('GET', 'track', params)
+    if kwargs[:email].to_s.empty? and kwargs[:id].to_s.empty?
+      raise KlaviyoError.new('You must identify a user by email or ID')
     end
 
-    def self.track_once(event, opts = {})
-      opts.update('__track_once__' => true)
-      track(event, opts)
-    end
+    customer_properties = kwargs[:customer_properties]
+    customer_properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
+    customer_properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
+
+    params = {
+      :token => @api_key,
+      :event => event,
+      :properties => kwargs[:properties],
+      :customer_properties => customer_properties
+    }
+    params[:time] = kwargs[:time].to_time.to_i if kwargs[:time]
+
+    request('GET', 'track', params)
+  end
+
+  def self.track_once(event, opts = {})
+    opts.update('__track_once__' => true)
+    track(event, opts)
   end
 end
