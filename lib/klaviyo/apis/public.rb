5c231cef6dc8b26a4a96e4d0a98bc55063fe749e
Chuck <chuck.curtis@klaviyo.com>
2020-11-23 16:30:35 -0500

    rename track to public class, consolidate track and identify into public class

--- lib/klaviyo/apis/public.rb
+++ lib/klaviyo/apis/public.rb
@@ -1,5 +1,33 @@
 module Klaviyo
-  class Track < Client
+  class Public < Client
+    # Used for identifying customers and managing profile properties
+    #
+    # @kwarg :id [String] the customer or profile id
+    # @kwarg :email [String] the customer or profile email
+    # @kwarg :properties [Hash] properties of the profile to add or update
+    def self.identify(kwargs = {})
+      defaults = {:id => nil,
+                  :email => nil,
+                  :properties => {}
+                 }
+      kwargs = defaults.merge(kwargs)
+
+      if !check_email_or_id_exists(kwargs)
+        return
+      end
+
+      properties = kwargs[:properties]
+      properties[:email] = kwargs[:email] unless kwargs[:email].to_s.empty?
+      properties[:id] = kwargs[:id] unless kwargs[:id].to_s.empty?
+
+      params = {
+        :token => Klaviyo.public_api_key,
+        :properties => properties
+      }
+
+      public_request(HTTP_GET, 'identify', params)
+    end
+
     # Used for tracking events and customer behaviors
     #
     # @param event [String] the event to track
