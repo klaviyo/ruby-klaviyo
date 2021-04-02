abdabb96bd1e8ad0ab67b6c2fdc039a74d154a8f
klaviyojad <jad.atoui@klaviyo.com>
2021-04-02 14:41:35 -0400

    add support for Data Privacy API and people/search endpoint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## CHANGELOG
 
+### 2.0.4
+
+* Add basic support for Data Privacy API and people/search endpoint
+
 ### 2.0.3
 
 * Add basic support for Campaigns API
---
+++ lib/klaviyo/apis/data_privacy.rb
@@ -0,0 +1,19 @@
+module Klaviyo
+  class DataPrivacy < Client
+    DATA_PRIVACY = 'data-privacy'
+    DELETION_REQUEST = 'deletion-request'
+    
+    # Submits a data privacy-related deletion request
+    # @param id_type [String] 'email' or 'phone_number' or 'person_id
+    # @param identifier [String] value for the identifier specified
+    # @return a dictionary with a confirmation that deletion task submitted for the customer
+    def self.request_profile_deletion(id_type, identifier)
+      unless ['email', 'phone_number', 'person_id'].include? id_type
+        raise Klaviyo::KlaviyoError.new(INVALID_ID_TYPE_ERROR)
+      end
+      identifier = { id_type => identifier }      
+      path = "#{DATA_PRIVACY}/#{DELETION_REQUEST}"
+      v2_request(HTTP_POST, path, identifier)
+    end
+  end
+end
