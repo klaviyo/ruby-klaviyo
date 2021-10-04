370ed224a3920481a6f5ce42d50787fcdae36635
Chuck Curtis <46108949+cpcurtis1218@users.noreply.github.com>
2021-10-04 13:31:44 -0400

    Add User-Agent and API key warnings (#45)
    * update gemspec and changelog
    
    * add user-agent, replace errors with warnings
    
    * update version variable
    
    * fix api key checks

--- lib/klaviyo/client.rb
+++ lib/klaviyo/client.rb
@@ -4,6 +4,9 @@ module Klaviyo
     V1_API = 'v1'
     V2_API = 'v2'
 
+    KL_VERSION = '2.1.0'
+    KL_USER_AGENT = "Ruby_Klaviyo/#{KL_VERSION}"
+
     HTTP_DELETE = 'delete'
     HTTP_GET = 'get'
     HTTP_POST = 'post'
@@ -29,7 +32,8 @@ module Klaviyo
       connection = Faraday.new(
         url: url,
         headers: {
-          'Content-Type' => content_type
+          'Content-Type' => content_type,
+          'User-Agent' => KL_USER_AGENT
       })
       if content_type == CONTENT_JSON
         kwargs[:body] = kwargs[:body].to_json
@@ -44,12 +48,13 @@ module Klaviyo
       if method == HTTP_GET
         params = build_params(kwargs)
         url = "#{BASE_API_URL}/#{path}?#{params}"
-        res = Faraday.get(url).body
+        res = Faraday.get(url, {}, { 'User-Agent' => KL_USER_AGENT }).body
       elsif method == HTTP_POST
         url = URI("#{BASE_API_URL}/#{path}")
         response = Faraday.post(url) do |req|
           req.headers['Content-Type'] = CONTENT_URL_FORM
           req.headers['Accept'] = 'text/html'
+          req.headers['User-Agent'] = KL_USER_AGENT
           req.body = {data: "#{kwargs.to_json}"}
         end
       else
@@ -112,9 +117,8 @@ module Klaviyo
         raise KlaviyoError.new(NO_PUBLIC_API_KEY_ERROR)
       end
       if ( Klaviyo.public_api_key =~ /pk_\w{34}$/ ) == 0
-        raise KlaviyoError.new(PRIVATE_KEY_AS_PUBLIC)
-      end
-      if ( Klaviyo.public_api_key =~ /\w{6}$/ ) != 0
+        warn(PRIVATE_KEY_AS_PUBLIC)
+      elsif ( Klaviyo.public_api_key =~ /\w{6}$/ ) != 0
         raise KlaviyoError.new(INCORRECT_PUBLIC_API_KEY_LENGTH)
       end
     end
