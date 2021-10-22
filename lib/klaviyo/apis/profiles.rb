811fb1fcb61e1c1a10065793485ad9b5133ff399
Chuck Curtis <chuck.curtis@klaviyo.com>
2021-10-22 13:18:56 -0400

    update profiles

--- lib/klaviyo/apis/profiles.rb
+++ lib/klaviyo/apis/profiles.rb
@@ -7,20 +7,20 @@ module Klaviyo
     # Retrieves the id of the profile given email
     # @param email [String] the email of the profile
     # @return [JSON] a JSON object containing id of the profile
-    def self.get_profile_id_by_email(email)
+    def self.get_profile_id_by_email(email, api_key: nil)
       path = "#{PEOPLE}/#{SEARCH}"
       params = {
         :email => email
       }
-      v2_request(HTTP_GET, path, **params)
+      v2_request(HTTP_GET, path, api_key: api_key, **params)
     end
 
     # Retrieve all the data attributes for a Klaviyo Person ID.
     # @param person_id [String] the id of the profile
     # @return returns a person object
-    def self.get_person_attributes(person_id)
+    def self.get_person_attributes(person_id, api_key: nil)
       path = "#{PERSON}/#{person_id}"
-      v1_request(HTTP_GET, path)
+      v1_request(HTTP_GET, path, api_key: api_key)
     end
 
     # Add or update one more more attributes for a Person
@@ -38,14 +38,14 @@ module Klaviyo
     # @param count [Integer] number of results to return, default 100
     # @param sort [String] 'asc' or 'desc', sort order to apply to the timeline.  Default is 'desc'.
     # @return returns a dictionary containing a list of metric event objects
-    def self.get_person_metrics_timeline(person_id, since: nil, count: DEFAULT_COUNT, sort: DEFAULT_SORT_DESC)
+    def self.get_person_metrics_timeline(person_id, since: nil, count: DEFAULT_COUNT, sort: DEFAULT_SORT_DESC, api_key: nil)
       path = "#{PERSON}/#{person_id}/#{METRICS}/#{TIMELINE}"
       params = {
         :since => since,
         :count => count,
         :sort => sort
       }
-      v1_request(HTTP_GET, path, **params)
+      v1_request(HTTP_GET, path, api_key: api_key, **params)
     end
 
     # Listing a person's event timeline for a particular metric
@@ -55,14 +55,14 @@ module Klaviyo
     # @param count [Integer] number of results to return, default 100
     # @param sort [String] 'asc' or 'desc', sort order to apply to the timeline.  Default is 'desc'.
     # @return returns a dictionary containing a list of metric event objects
-    def self.get_person_metric_timeline(person_id, metric_id, since: nil, count: DEFAULT_COUNT, sort: DEFAULT_SORT_DESC)
+    def self.get_person_metric_timeline(person_id, metric_id, since: nil, count: DEFAULT_COUNT, sort: DEFAULT_SORT_DESC, api_key: nil)
       path = "#{PERSON}/#{person_id}/#{METRIC}/#{metric_id}/#{TIMELINE}"
       params = {
         :since => since,
         :count => count,
         :sort => sort
       }
-      v1_request(HTTP_GET, path, **params)
+      v1_request(HTTP_GET, path, api_key: api_key, **params)
     end
   end
 end
