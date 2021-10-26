4679b1de802f6047485266b2e21a0f5fcf41e1be
Chuck Curtis <chuck.curtis@klaviyo.com>
2021-10-26 10:33:42 -0400

    add api key kwarg and docstring

--- lib/klaviyo/apis/data_privacy.rb
+++ lib/klaviyo/apis/data_privacy.rb
@@ -6,14 +6,15 @@ module Klaviyo
     # Submits a data privacy-related deletion request
     # @param id_type [String] 'email' or 'phone_number' or 'person_id
     # @param identifier [String] value for the identifier specified
+    # @kwarg :api_key [String] private API key for this request
     # @return a dictionary with a confirmation that deletion task submitted for the customer
-    def self.request_profile_deletion(id_type, identifier)
+    def self.request_profile_deletion(id_type, identifier, api_key: nil)
       unless ['email', 'phone_number', 'person_id'].include? id_type
         raise Klaviyo::KlaviyoError.new(INVALID_ID_TYPE_ERROR)
       end
       data = Hash[id_type.to_sym, identifier]
       path = "#{DATA_PRIVACY}/#{DELETION_REQUEST}"
-      v2_request(HTTP_POST, path, **data)
+      v2_request(HTTP_POST, path, api_key: api_key, **data)
     end
   end
 end
